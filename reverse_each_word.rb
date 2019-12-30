cf6dda624bc757712389d16d347f50c25e3e3ffb
Douglas Johnson <douglas.j.johnson@gmail.com>
2019-12-30 16:57:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,11 @@ def reverse_each_word(sentence)
   return_sentence = ""
   word_array = sentence.split(' ')
 
-  word_array.each do |word|
-    word.reverse!
-  end
+  #word_array.each do |word|
+  #  word.reverse!
+  #end
+
+  word_array.collect(|word| word.reverse)
 
   return_sentence = word_array.join(' ')
 
